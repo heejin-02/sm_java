b64363e09d85dd0899a68a3459c2dd91ac0d4251
smhrd1 <smhrd1@192.168.219.58>
2025-03-10 10:23:47 +0900

    java day1~6

---
+++ Ex05배열/src/이차원배열실습/Ex01기초예제.java
@@ -0,0 +1,33 @@
+package 이차원배열실습;
+
+public class Ex01기초예제 {
+
+	public static void main(String[] args) {
+		int[][] array2 = { 
+				{ 1, 2, 3, 4 }, // 0행
+				{ 5, 6, 7, 8 }, // 1행
+				{ 9, 10, 11, 12 } // 2행
+		};
+
+		// for문 써서 1,2,3,4 출력
+		// array2.length : array2행의 갯수 -> 2
+		// array2[0].length : array2의 0번 행이 가지고 있는 데이터의 갯수 -> 4
+		for (int i = 0; i<array2[0].length; i++) {
+			System.out.print(array2[0][i] + " ");
+		}
+		System.out.println();
+		//for문 써서 5,6,7,8 출력
+		for (int i =0; i<array2[1].length; i++) {
+			System.out.print(array2[1][i] + " ");
+		}
+		System.out.println();
+		//for문 써서 9.10,11,12 출력
+		for (int i=0; i<array2[2].length; i++) {
+			System.out.print(array2[2][i] + " ");
+		}
+		//for문이 총 3개
+		
+		
+	}
+
+}
