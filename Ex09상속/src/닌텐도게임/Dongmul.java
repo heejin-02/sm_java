6b3641cdde70c1f5ef023109b9ae74b53f100386
smhrd1 <lumosmaxima_@naver.com>
2025-03-13 15:07:14 +0900

    추상클래스 실습

--- Ex09상속/src/닌텐도게임/Dongmul.java
+++ Ex09상속/src/닌텐도게임/Dongmul.java
@@ -11,7 +11,4 @@ public class Dongmul extends GameChip {
 	
 
 	
-	
-	
-	
 }
---
+++ Ex10추상클래스/src/추상클래스/Main.java
@@ -0,0 +1,11 @@
+package 추상클래스;
+
+public class Main {
+
+	public static void main(String[] args) {
+		RegularEmployee r = new RegularEmployee("SMHRD001", "홍길동", 4000, 100);
+		System.out.println(r.print());
+
+	}
+
+}
