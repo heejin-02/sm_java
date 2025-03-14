fcb00a706fe613cb83547cfe530071afe39a8f82
smhrd1 <lumosmaxima_@naver.com>
2025-03-14 14:47:27 +0900

    로그인 실습

--- EX11JDBC/src/JDBC기초/Ex01Join.java
+++ EX11JDBC/src/JDBC기초/Ex01Join.java
@@ -1,11 +1,29 @@
 package JDBC기초;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Ex01Join {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("ID 입력 : ");
+		String id = sc.next();
+		System.out.println("PW 입력 : ");
+		String pw = sc.next();
+		System.out.println("이름 입력 : ");
+		String name = sc.next();
+		System.out.println("나이 입력 : ");
+		int age = sc.nextInt();
+		System.out.println("점수 입력 : ");
+		int score = sc.nextInt();
 
 		//JDBC(Java Database Connectivity)
 		//: JDBC 인터페이스 사용 방법
-		
+		PreparedStatement psmt = null;
+		Connection conn = null;
 		try {
 			//try 구문
 			// -> 예외가 발생할 수도 있지만, 우선 시도해보는 구간!
@@ -26,17 +44,71 @@ public class Ex01Join {
 			// -> 원하는 jar파일 경로 선택 -> apply and close
 			
 			
-			// 2. 데이터베이스 연결
-			// 준비물 3가지
-			// 1) 연결 경로
+//			 2. 데이터베이스 연결
+//			 준비물 3가지
+//			 1) 연결 경로
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			//JDBC 드라이버가 thin 타입이다.
-			//@localhost --> ip주소(127...)
-			// 2) 계정 이름
-			// 3) 계정 비밀번호
+//			JDBC 드라이버가 thin 타입이다.
+//			@localhost --> ip주소(127.0.0.1)
+//			1521 --> port번호 
+//			xe --> DB 기본 이름
+//			 2) 계정 이름
+			String user ="hr";
+//			 3) 계정 비밀번호
+			String password = "hr";
+			
+			conn = DriverManager.getConnection(url, user, password);
+			//Connection이란?
+			//: 데이터베이스에 접속하기 위한 모든 메소드를 가진 인터페이스
+			// ---> 데이터베이스와 하는 모든 통신은 Connection을 통해서만 이루어진다.
+			// Connection == MetaData(거대한 데이터)
+			// ---> 사용 가능한 테이블 목록, 컬럼들에 대한 정보 등
+			// 		연결된 데이터베이스의 정보들을 전부 가지고 있음.
+			
+			if(conn != null) {
+				System.out.println("연결 성공!");
+			}
+			else {
+				System.out.println("연결 실패 ...");
+			}
+			
+			// 3. Query문(SQL구문) 전송
+			String sql = "INSERT INTO DATADESIGNMEMBER VALUES(?,?,?,?,?) ";
+			
+			// ? 인자
+			// : 사용자로부터 어떤 데이터가 들어올지 모를 때 사용
+			// : 지정한 데이터 공간만큼 비워놓고 실행하기 전까지 대기
+			
+			conn.prepareStatement(sql);
+			psmt = conn.prepareStatement(sql);
+			
+			// ? 인자 채우는 작업!!
+			// 데이터 바인딩(data binding) 작업
+			psmt.setString(1, id);
+			psmt.setString(2, pw);
+			psmt.setString(3,name);
+			psmt.setInt(4, age);
+			psmt.setInt(5,score);
+			
+			//4. sql문 실행
+			int row = psmt.executeUpdate();
+			// executeUpdate() : insert, update, delete에만 사용한다.
+			
+			//5. 결과를 이용한 작업처리
+			if(row > 0) {
+				System.out.println("회원가입 성공~");
+			}else {
+				System.out.println("회원가입 실패 ㅜㅜ");
+			}
 			
 			
-		} catch (ClassNotFoundException e) {
+		} catch (Exception e) {
+			//catch (ClassNotFoundException | SQLException e) {
+			// ---> 모든 예외 클래스는 Exception을 상속받는다.
+			// ---> Exception e = new ClassNotFoundException();
+			// --------> 업캐스팅이 일어나는 개념
+			
+			
 			// catch 구문
 			// --> 예외가 발생했을 때, 어떤 로직을 수행할 지 작성하는 구간!
 			// --> ()안에 있는 매개변수는, 어떤 예외상황을 잡아줄건지 기술하는 공간!
@@ -45,6 +117,20 @@ public class Ex01Join {
 			// 출력해주는 구문!! --> 개발할 때만 사용하기
 			// --> 배포할 떄는 삭제하는 것이 좋음!
 			
+		} finally {
+			// 위에서 예외 상황이 발생하더라도 반드시 한번은 들어오는 구간
+			// 6. DB 연결 종료 --> 자원 반납
+				try {
+					// 자원 반납 시 항상 사용한 순서의 역순으로 반납한다! 
+					if(psmt != null)
+						psmt.close();
+					if(conn != null)
+						conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		}
+		
 	}
 }
